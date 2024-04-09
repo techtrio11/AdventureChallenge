5f95e08214f0acb7e5381742f1db3d94c2cdd899
amycmoore <techtrio11@gmail.com>
2024-04-08 18:53:52 -0600

    update

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+//TypeScript for all Stack Routes
+import { RootStackParams } from "./src/types";
+
+//Stack Navigation Screens
+import ChooseLocation from "./src/routes/chooseLocationStack";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="ChooseLocation">
+        <Drawer.Screen
+          name="ChooseLocation"
+          component={ChooseLocation}
+          options={{
+            drawerLabel: "Choose Location",
+            title: "Choose Location",
+          }}
+        />
+        {/* <Drawer.Screen
+          name="OfficerPortal"
+          component={OfficerPortalStack}
+          options={{
+            drawerLabel: "Vehicle List",
+            title: "Vehicle List",
+          }}
+        /> */}
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # SaveRight
 
 ## clone the app
+
 Prior to cloing the app make sure your terminal is in the parent directory.
+
 ```bash
 git clone <https>
 ```
+
 After cloing the project, open the project folder in VSCode.
 
 ## install react native
@@ -15,6 +18,22 @@ npm install --global eas-cli
 
 ```bash
 npm add expo
+```
+
+```bash
+npm install @react-navigation/native
+```
+
+```bash
+npm install @react-navigation/drawer
+```
+
+```bash
+npm install @react-navigation/native-stack
+```
+
+```bash
+npm install react-native-dropdown-select-list
 ```
 
 ## run the app
