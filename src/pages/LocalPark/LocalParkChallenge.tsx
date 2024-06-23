b255ba4ba48c4cf51a091417eb3ffeb377fafd13
techtrio11 <techtrio11@gmail.com>
2024-06-23 16:49:12 -0600

    made the picture able to appear and disappear on all screens

--- src/pages/LocalPark/LocalParkChallenge.tsx
+++ src/pages/LocalPark/LocalParkChallenge.tsx
@@ -1,5 +1,10 @@
-import { Text } from "react-native";
-import { ContainerCenter } from "../../components";
+import { Text, Image } from "react-native";
+import { ContainerCenter, SolidButton } from "../../components";
+import { buttonStyles, globalStyles } from "../../styles";
+import { useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+import { ref, uploadBytes } from "firebase/storage";
+import { storage } from "../../../FirebaseConfig";
 
 type Props = {
   navigation: any;
@@ -8,12 +13,48 @@ type Props = {
 
 const LocalParkChallenge = ({ navigation, route }: Props) => {
   const { challengeId, challengeName, challengeDescription } = route.params;
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0]);
+    }
+  };
+  const uploadImage = async () => {
+    const storageRef = ref(storage, image.fileName);
+    const img = await fetch(image);
+    const bytes = await img.blob();
+    uploadBytes(storageRef, bytes).then(() => {
+      setImage(null);
+    });
+  };
+
   return (
     <ContainerCenter>
       <>
         <Text>{challengeId}</Text>
         <Text>{challengeName}</Text>
         <Text>{challengeDescription}</Text>
+        <SolidButton
+          buttonText="Pick an image from camera roll"
+          onPress={pickImage}
+          pressableColor={buttonStyles.solidYellowButton}
+        />
+        {image && image.uri && (
+          <Image source={{ uri: image.uri }} style={globalStyles.image} />
+        )}
+        <SolidButton
+          buttonText="Complete Challenge"
+          onPress={uploadImage}
+          pressableColor={buttonStyles.solidGreenButton}
+        />
       </>
     </ContainerCenter>
   );
