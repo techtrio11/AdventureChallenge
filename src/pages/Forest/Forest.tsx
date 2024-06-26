d1c7195c4d0fa4de5688f734d17630dfa167ed97
techtrio11 <techtrio11@gmail.com>
2024-06-26 10:44:03 -0600

    updated the userChallengesAvailable on Forest, LocalPark and Water.

--- src/pages/Forest/Forest.tsx
+++ src/pages/Forest/Forest.tsx
@@ -1,17 +1,29 @@
 import { Text, View } from "react-native";
 import { ContainerCenter, SolidButton, TextButton } from "../../components";
-import { challengesReference } from "../../../FirebaseConfig";
+import { challengesReference, usersReference } from "../../../FirebaseConfig";
 import { useEffect, useState } from "react";
 import { ChallengesData } from "../../types";
-import { query, onSnapshot, collection, where } from "firebase/firestore";
-import { availableChallenges, getRandomChallenges } from "../../utils";
+import { query, onSnapshot, where } from "firebase/firestore";
+import {
+  getFilteredChallengeData,
+  availableChallenges,
+  getRandomChallenges,
+} from "../../utils";
 import { buttonStyles, globalStyles } from "../../styles";
 
 type Props = {
   navigation: any;
+  route: any;
 };
 
-const Forest = ({ navigation }: Props) => {
+const Forest = ({ navigation, route }: Props) => {
+  const { userId } = route.params;
+  const [userCompletedChallenges, setUserCompletedChallenges] = useState<
+    ChallengesData[]
+  >([]);
+  const [userChallengesAvailable, setUserChallengesAvailable] = useState<
+    ChallengesData[]
+  >([]);
   const [challengeData, setChallengesData] = useState<ChallengesData[]>([]);
   const [selectedOptions, setSelectedOptions] = useState<ChallengesData[]>([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -52,16 +64,52 @@ const Forest = ({ navigation }: Props) => {
     };
   }, []);
 
+  //get completed challenges from database
+  useEffect(() => {
+    const fetchData = async () => {
+      const usersQuery = query(usersReference);
+      const completedChallenges = onSnapshot(usersQuery, (querySnapshot) => {
+        const list = [];
+        querySnapshot.forEach((doc) => {
+          if (doc.id === userId) {
+            const data = doc.data();
+            data.activities_completed.forEach((activity) => {
+              list.push({
+                id: activity.activity_id,
+              });
+            });
+          }
+        });
+        if (list.length) setUserCompletedChallenges(list);
+        setIsLoading(false);
+      });
+
+      return completedChallenges;
+    };
+    fetchData();
+
+    // Cleanup function
+    return () => {
+      // Perform any cleanup here if needed
+    };
+  }, [userId]);
+
+  //get all the user challenges available, filtering out completed challenges
+  useEffect(() => {
+    setUserChallengesAvailable(
+      getFilteredChallengeData(challengeData, userCompletedChallenges)
+    );
+  }, [challengeData, userCompletedChallenges]);
+
   //get initial challenges
   useEffect(() => {
     //TO DO: pass in user completed data
     setSelectedOptions(getRandomChallenges(challengeData));
-    setAvailableChallengesData(availableChallenges(challengeData));
   }, [challengeData]);
 
   return (
     <ContainerCenter>
-      {isLoading ? (
+      {isLoading && selectedOptions.length ? (
         <>Loading data ...</>
       ) : (
         <>
@@ -87,7 +135,7 @@ const Forest = ({ navigation }: Props) => {
                 />
                 {isFirstButton && selectedOptions.length == 2 && (
                   <Text
-                    key={`forest_OR_${index}`}
+                    /* key={`forest_OR_${index}`}*/
                     style={globalStyles.mainHeader}
                   >
                     OR
@@ -96,16 +144,18 @@ const Forest = ({ navigation }: Props) => {
               </View>
             );
           })}
-          {availableChallengeData.length > 2 && (
+          {selectedOptions.length > 2 && (
             <TextButton
               buttonText="Pick Again"
               onPress={() => {
                 setSelectedOptions((prevOptions) =>
-                  getRandomChallenges(challengeData, prevOptions)
+                  getRandomChallenges(userChallengesAvailable, prevOptions)
                 );
               }}
             />
           )}
+          {/* TO DO: What to do if challengeData is empty? */}
+          {/* TO DO: How does a user choose a different challenge location? */}
         </>
       )}
     </ContainerCenter>
